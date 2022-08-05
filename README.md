e67b845fc5f1cb62568e04dd9c89b69ff480e8ec
zxp-yuluo <zxp_10012991@163.com>
2022-08-05 14:14:32 +0800

    启动页完成

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # 腻歪音乐
+音乐小程序练习
+
+## 启动页
+
+**guidance文件夹**
+
+>使用过渡和小程序动画API完成
+
+**动画API简单使用**
+
+```js
+// JS文件
+Page({
+  data: {
+    animationData: {}      // 动画对象
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    // 创建一个动画实例
+	var animation = wx.createAnimation({
+      duration: 4000,
+      timingFunction: 'ease',
+      delay: 1000
+    })
+    // 调用动画实例的方法
+    animation.opacity(0.2).translate(100, 100).step()
+
+    this.setData({
+      animationData: animation.export()
+    })
+  },
+})
+```
+
+```html
+<!--wxml文件-->
+<view animation="{{animationData}}">动画API简单使用</view>
+```
+
+**图片展示**
+
+| ![](picture\启动页01.png) | ![](picture\启动页02.png) |
+| ------------------------- | ------------------------- |
+
---
+++ pages/guidance/guidance.js
@@ -0,0 +1,64 @@
+// pages/guidance/guidance.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tab: true, // true：显示第一屏, false：显示第二屏
+    oneStart: false, // 第一屏动画是否开始
+    twoStart: false,
+    nextAni: '', // 下一页动画
+    amplifyAni: '', // 放大动画
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    // 点击下一屏的动画
+    const nextAni = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease'
+    })
+    this.clearId = setInterval(() => {
+      nextAni.opacity(0).step()
+      nextAni.opacity(1).step()
+      this.setData({
+        nextAni: nextAni.export()
+      })
+    }, 2000)
+  },
+  // logo图片加载完成
+  loadding() {
+    this.setData({
+      oneStart: true
+    })
+  },
+  // 点击下一页
+  handleNext() {
+    clearInterval(this.clearId)
+    this.setData({
+      tab: false,
+    }, () => {
+      setTimeout(() => {
+        const amplifyAni = wx.createAnimation({
+          duration: 2000,
+          timingFunction: 'ease',
+        })
+        amplifyAni.opacity(1).scale(1).step()
+        // Animation.
+        this.setData({
+          amplifyAni: amplifyAni.export(),
+          twoStart: true
+        })
+      }, 2000)
+    })
+  },
+  // 跳转页面
+  handleSkip() {
+    wx.redirectTo({
+      url: '/pages/home/home',
+    })
+  }
+})
